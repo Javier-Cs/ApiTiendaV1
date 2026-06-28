04f4ec9a8012348cea790c11004a61dc36ba66bc
Javier Carchipulla Sisalima <krchipullajavier@gmail.com>
2026-06-28 13:52:59 -0500

    implemetacion de repo intentos de login intentos de login por ip intentos fallidos limpiar intentos fallidos

---
+++ ApiTiendaV1/Repositorios/Auth/ContarIntentosRepo.cs
@@ -0,0 +1,103 @@
+﻿using ApiTiendaV1.Data;
+using Dapper;
+
+namespace ApiTiendaV1.Repositorios.Auth
+{
+    public class ContarIntentosRepo
+    {
+        private readonly ISqlConnectionFactory _sqlConnectionFactory;
+
+        public ContarIntentosRepo(ISqlConnectionFactory sqlConnectionFactory) {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        // contar intentos del login
+        public async Task<int> ContarIntentosUltimosMinutos(string email, int minutos = 60) 
+        {
+            const string sql = @"
+                SELECT COUNT(*)
+                FROM login_attempts
+                WHERE email = @email
+                    AND fecha >=  DATEADD(MINUTE, -@minutos, GETDATE());
+            ";
+
+            using var connection = _sqlConnectionFactory.CreateConnection();
+            return await connection.ExecuteScalarAsync<int>(
+                sql,
+                new 
+                {
+                    email,
+                    minutos
+
+                }
+            );
+        }
+
+
+
+        // contar intentos por ip
+        public async Task<int> ContarIntentosPorIp(string ip, int minutos = 60) {
+            const string sql = @"
+                SELECT COUNT(*)
+                FOM login_attempts
+                WHERE ip = @ip
+                AND fecha >= DATEADD(MINUTE, -@minutos, GETDATE())
+            ";
+
+            using var connection = _sqlConnectionFactory.CreateConnection();
+            return await connection.ExecuteScalarAsync<int>(
+                sql,
+                new { ip, minutos }
+            );
+        }
+
+
+        // guardar intentos fallidos 
+        public async Task RegistraIntentosFallidos(string email, string? ip = null) {
+            const string sql = @"
+                INSERT INTO login_attempts
+                (
+                    email,
+                    fecha,
+                    ip
+                )VALUES(
+                    @email,
+                    GETDATE(),
+                    @ip
+                );
+            ";
+
+            using var connection = _sqlConnectionFactory.CreateConnection();
+            await connection.ExecuteAsync(
+                sql,
+                new { 
+                    email,
+                    ip
+                }
+            );
+        }
+
+
+
+
+        // limpiar intentos cuando el login es correcto
+        public async Task LimpiarIntentos(string email)
+        {
+            const string sql = @"
+                DELETE FROM login_attempts
+                WHERE email = @email;
+            ";
+
+            using var connection = _sqlConnectionFactory.CreateConnection();
+            await connection.ExecuteAsync(
+                sql,
+                new { email}
+            );
+        }
+
+
+
+
+
+    }
+}
