bc4e820d8f230aa65b295fdb8e16b9d7c0f0f2bb
Javier Carchipulla Sisalima <krchipullajavier@gmail.com>
2026-06-28 12:55:24 -0500

    implementacion de token y su caracteristicas

---
+++ ApiTiendaV1/DTOs/Auths/Usuario.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiTiendaV1.DTOs.Auths
+{
+    public class Usuario
+    {
+        public int id_usuario { get; set; }
+        public string? nombre { get; set; }
+        public string? rol { get; set; }
+        public string? email { get; set; }
+        public string? passhass { get; set; }
+        public bool estado { get; set; }
+        public bool is_deleted { get; set; }
+        public string? telefono { get; set; }
+        public DateTime fecha_creacion { get; set; }
+    }
+}
---
+++ ApiTiendaV1/Validation/JWTService.cs
@@ -0,0 +1,63 @@
+﻿using ApiTiendaV1.DTOs.Auths;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ApiTiendaV1.Validation
+{
+    public class JWTService
+    {
+
+        IConfiguration _config;
+
+        public JWTService(IConfiguration config) {
+            _config = config;
+        }
+
+        // generar Token
+
+        public string GenerarToken(Usuario user, DateTime expiracion) {
+
+            //datos que vas dentro del token
+            var claims = new[]{
+                new Claim(ClaimTypes.NameIdentifier, user.id_usuario.ToString()),
+                new Claim(ClaimTypes.Name, user.nombre),
+                new Claim(ClaimTypes.Role, user.rol),
+                new Claim(ClaimTypes.Email, user.email),
+
+                new Claim(
+                    JwtRegisteredClaimNames.Jti,
+                    Guid.NewGuid().ToString()
+                ),
+
+                new Claim(
+                    JwtRegisteredClaimNames.Iat,
+                    DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString(),
+                    ClaimValueTypes.Integer64
+                )
+            };
+
+            //crear la llave
+            var key = new SymmetricSecurityKey(
+                // convierte a la llave en bytes de los datos de appsettings.json
+                Encoding.UTF8.GetBytes(_config["Jwt:key"])
+            );
+            // crea las credenciales de la llave y se firma
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+
+            // crea el token
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issure"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: expiracion,
+                signingCredentials: cred
+            );
+
+            // se escribe el token y se retorma en texto
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
